05381a7cac55e015d821337a7c04bde3be96198c
Mcproco <masonmallillin@gmail.com>
2026-03-11 00:36:00 -0700

    feat: its due at 11:59PM tomorrow, not today

---
+++ js/linktr.js
@@ -0,0 +1,39 @@
+const linktr = document.getElementById("linktr");
+const directories = {
+    "HOME" : "/",
+    "COMMUNITY" : "community/",
+    "FORMS" : "forms/",
+    "PAPER" : "paper/",
+    "PROJECT" : "project/",
+    "SOURCES" : "sources/",
+    "LOGS" : "logs/",
+    "REFLECTION" : "reflection/"
+}
+
+function populate(name, dir) {
+
+    const obj = document.createElement("a");
+    obj.text = `${name}`;
+    obj.href = dir
+
+    return obj;
+
+}
+
+function init() {
+
+    let objs = [];
+
+    for (key in directories) {
+
+        let obj = populate(key, directories[key]);
+        objs.push(obj);
+
+    }
+
+    for (i in objs) {
+        linktr.appendChild(objs[i]);
+    }
+}
+
+init();
